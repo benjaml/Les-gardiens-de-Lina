1f09ed85ba27aaecd5f97e47137356556faddcc6
benjaml <b.lefevre@rubika-edu.com>
2016-05-09 19:23:30 +0200

    Push of player movement and aim

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public float deadZone;
+    private CharacterController controller;
+
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+        controller.Move(new Vector3(h, 0.0f, v) * speed * Time.deltaTime);
+
+        
+        v = Input.GetAxisRaw("VerticalLook");
+        if (Mathf.Abs(v) < deadZone)
+            v = 0f;
+        h = Input.GetAxisRaw("HorizontalLook");
+        if (Mathf.Abs(h) < deadZone)
+            h = 0f;
+        transform.LookAt(transform.position+new Vector3(h, 0.0f, v));
+    }
+}
