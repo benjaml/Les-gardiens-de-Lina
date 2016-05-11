3664da70913074e05c2e372cc1ee8c9e87df2516
benjaml <b.lefevre@rubika-edu.com>
2016-05-11 22:48:50 +0200

    Add of menus still need to put graphs

---
+++ Assets/CreditScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CreditScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButton("Cancel"))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+	}
+}
---
+++ Assets/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+
+    public void SetCredit()
+    {
+        SceneManager.LoadScene("Crédit");
+    }
+
+    public void SetMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void SetGame()
+    {
+        SceneManager.LoadScene("Tuto");
+    }
+
+
+}
