811ebe276323f50f0d9682fbd9d4cb6666c4f7ac
benjaml <b.lefevre@rubika-edu.com>
2016-05-12 20:06:31 +0200

    dez

--- Assets/deadZone.cs
+++ Assets/deadZone.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class deadZone : MonoBehaviour {
+public class Deadzone : MonoBehaviour {
 
 	void OnTriggerEnter(Collider col)
     {
-        if (col.transform.tag == "Enemy")
+        if(col.transform.tag == "Enemy")
+        {
             col.GetComponent<Enemy>().Death();
+        }
     }
 }
